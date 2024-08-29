277e46af818bd52f8c13fbf564f3d9fbe6521aca
ali-talal <ali.talalfakhri09@gmail.com>
2024-08-29 20:57:15 +0300

    final

--- scripts/section-five.js
+++ scripts/section-five.js
@@ -3,31 +3,37 @@ import { offers } from "./offers.js";
 let html = '';
 
 offers.forEach((offer) => {
-    // Create an HTML string for sizes and prices
     let sizesHtml = '';
-    for (let i = 0; i < offer.length; i++) {
+    for (let i = 0; i < offer.sizes.length; i++) {
         sizesHtml += `
-        
-            <div class="size-box" onclick="window.location = 'https://wa.me/+9647770641171?text= ${offer.name}/   ${offer.prices[i]}/ (${ offer.sizes[i]}) اريد هذا العرض';">
+            <div class="size-box" onclick="window.location = 'https://wa.me/+9647770641171?text=${offer.name}/ ${offer.prices[i]}/ (${offer.sizes[i]}) اريد هذا العرض';">
                 <h2>${offer.sizes[i]} :القياسات</h2>
                 <h3>${offer.prices[i].toLocaleString()} :السعر</h3>
-             </div>
-        
+            </div>
         `;
     }
 
-    // Generate the HTML for the entire offer
     html += `
         <div class="card">
             <h2 class="offer-name">${offer.name}</h2>
             <h2>علاكة لون واحد 1000</h2>
             <img class="image" src="${offer.Image}" alt="${offer.name}">
-            <div class="sizes" >
+            <div class="sizes">
                 ${sizesHtml}
             </div>
         </div>
     `;
 });
 
-// Assuming you have an element with the class 'offers-cont' to insert the generated HTML
-document.querySelector('.offers-cont').innerHTML = html;
+// Ensure the DOM is loaded before querying elements
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.offers-cont').innerHTML = html;
+
+    document.querySelectorAll('.image').forEach((img) => {
+        img.addEventListener('click' , function(){
+            const imageUrl = encodeURIComponent(this.src);
+            window.location.href = `images.html?image=${imageUrl}`;
+        })
+      
+    });
+});
